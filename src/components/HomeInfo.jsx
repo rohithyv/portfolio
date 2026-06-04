74d9ab80fdb7b50b9854c99f1ac80a0f3f94fb56
Rohith Yellambalase Vijayakumar <53818270+rohithyv@users.noreply.github.com>
2026-06-04 16:38:58 -0400

    Refactor HomeInfo component for better readability

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
-
 import { arrow } from "../assets/icons";
 
 const HomeInfo = ({ currentStage }) => {
-if (currentStage === 1)
-  return (
-    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-      Hi, I'm
-      <span className='font-semibold mx-2 text-white'>Rohith Y V</span>
-      👋
-      <br />
-      A Software Engineer from United States 🇺🇸
-    </h1>
-  );
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Rohith Y V</span>
+        👋
+        <br />
+        A Software Engineer from United States 🇺🇸
+      </h1>
+    );
 
   if (currentStage === 2) {
     return (
@@ -20,7 +19,6 @@ if (currentStage === 1)
         <p className='font-medium sm:text-xl text-center'>
           Worked with many companies <br /> and picked up many skills along the way
         </p>
-
         <Link to='/about' className='neo-brutalism-white neo-btn'>
           Learn more
           <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
@@ -35,7 +33,6 @@ if (currentStage === 1)
         <p className='font-medium text-center sm:text-xl'>
           Led multiple projects to success over the years. <br /> Curious about the impact?
         </p>
-
         <Link to='/projects' className='neo-brutalism-white neo-btn'>
           Visit my portfolio
           <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
@@ -44,28 +41,28 @@ if (currentStage === 1)
     );
   }
 
-if (currentStage === 4) {
-  return (
-    <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
-      </p>
-      <div className='flex gap-3'>
-        
-          href='/Resume_Rohith.pdf'
-          download='Resume_Rohith.pdf'
-          className='neo-brutalism-white neo-btn'
-        >
-          ⬇ Resume
-        </a>
-        <Link to='/contact' className='neo-brutalism-white neo-btn'>
-          Let's talk
-          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-        </Link>
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
+        </p>
+        <div className='flex items-center gap-3'>
+          
+            href='/Resume_Rohith.pdf'
+            download='Resume_Rohith.pdf'
+            className='neo-brutalism-white neo-btn'
+          >
+            ⬇ Resume
+          </a>
+          <Link to='/contact' className='neo-brutalism-white neo-btn'>
+            Let's talk
+            <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
   return null;
 };
